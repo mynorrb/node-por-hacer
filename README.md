3a730757a682f38e2b7c290baea8fcd74fe6695c
Mynor Reyes <mynorrb@gmail.com>
2020-06-13 09:23:33 -0600

    primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Aplicación de comandos
+
+Este es el ejercicios de curso
+
+Recuerden instalar los paquetes de node
+
+````
+npm install
+```
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guaradarDB = () => {
+
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error('No se puedo grabar', err);
+    });
+};
+
+const cargarDB = () => {
+
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+
+    }
+
+
+};
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+
+    guaradarDB();
+
+    return porHacer;
+};
+
+const getListado = () => {
+    cargarDB();
+
+    return listadoPorHacer;
+};
+
+const actualizar = (descripcion, completado = true) => {
+    cargarDB();
+
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    if (index >= 0) {
+
+        listadoPorHacer[index].completado = completado;
+        guaradarDB();
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const borrar = (descripcion) => {
+
+    cargarDB();
+
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    if (index >= 0) {
+
+        listadoPorHacer.splice(index, 1);
+        guaradarDB();
+        return true;
+    } else {
+        return false;
+    }
+};
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+};
